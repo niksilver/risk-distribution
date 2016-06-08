8f10dae59d192a26fe5fea8314e88829a6cdf60f
Nik Silver <nik@niksilver.com>
2016-06-08 20:37:38 +0100

    Chart's SVG element is now a full width

--- src/Chart.elm
+++ src/Chart.elm
@@ -4,7 +4,7 @@ import FactList
 
 import Html exposing (Html)
 import Svg exposing (svg, text', text)
-import Svg.Attributes exposing (width, height, viewBox, x, y)
+import Svg.Attributes exposing (width, height, x, y)
 
 
 type alias Model = FactList.Model
@@ -13,12 +13,11 @@ type alias Model = FactList.Model
 view : Model -> Html x
 view model =
     svg
-    [ width "800px"
+    [ width "100%"
     , height "600px"
-    , viewBox "0 0 800 600"
     ]
     [ text'
-      [ x "100"
+      [ x "0"
       , y "100"
       ]
       [ model |> toString |> text
