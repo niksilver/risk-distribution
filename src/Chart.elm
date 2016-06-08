757a097091e7b0764eaeb5c16122d081f76ff78c
Nik Silver <nik@niksilver.com>
2016-06-08 20:32:34 +0100

    Started adding making the Chart view an SVG element.

--- src/Chart.elm
+++ src/Chart.elm
@@ -2,13 +2,25 @@ module Chart exposing (view)
 
 import FactList
 
-import Html exposing (Html, p, text)
+import Html exposing (Html)
+import Svg exposing (svg, text', text)
+import Svg.Attributes exposing (width, height, viewBox, x, y)
 
 
 type alias Model = FactList.Model
 
+
 view : Model -> Html x
 view model =
-    p []
-    [ model |> toString |> text
+    svg
+    [ width "800px"
+    , height "600px"
+    , viewBox "0 0 800 600"
+    ]
+    [ text'
+      [ x "100"
+      , y "100"
+      ]
+      [ model |> toString |> text
+      ]
     ]
