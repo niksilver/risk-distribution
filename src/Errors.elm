8806cc65ac73a0c4fd158ee6f75e8111887d8508
Nik Silver <nik@niksilver.com>
2016-06-15 22:25:36 +0100

    Can correctly recognise a 'more than 100%' error

--- src/Errors.elm
+++ src/Errors.elm
@@ -1,6 +1,6 @@
-module Errors exposing (errors)
+module Errors exposing (Error (MoreThan100Percent), errors)
 
-import Distribution as Dist exposing (Layer)
+import Distribution as Dist exposing (Layer, Limit (AtLeast, AtMost))
 
 -- Kinds of error
 
@@ -11,5 +11,32 @@ type Error
 
 errors : List Layer -> List Error
 errors ys =
-    []
+    List.concat
+    [ moreThan100PercentErrors ys
+    ]
+
+exists : (a -> Bool) -> List a -> Bool
+exists pred xs =
+    case xs of
+        [] ->
+            False
+        head :: tail ->
+            if (pred head) then True else exists pred tail
+
+moreThan100PercentErrors : List Layer -> List Error
+moreThan100PercentErrors ys =
+    let
+        divergent y1 y2 =
+            (y1.limit == AtMost && y2.limit == AtLeast && y1.value < y2.value)
+        over100Pc y1 y2 =
+            (y1.value + y2.value > 1.00)
+        isErrorCounterpart y1 y2 =
+            (divergent y1 y2) && (over100Pc y1 y2)
+        hasErrorCounterpart y1 =
+            exists (isErrorCounterpart y1) ys
+    in
+        if (exists hasErrorCounterpart ys) then
+            [ MoreThan100Percent ]
+        else
+            []
 
--- tests/ErrorsTest.elm
+++ tests/ErrorsTest.elm
@@ -23,5 +23,44 @@ errorsTest =
           assertEqual
           []
           (errors [y1, y2])
+
+    , test "Simple bounds plus okay layer should give no errors" <|
+      let
+          y1 = { prob = 1.00, limit = AtLeast, value = 20.0 }
+          y2 = { prob = 0.50, limit = AtLeast, value = 30.0 }
+          y3 = { prob = 1.00, limit = AtMost, value = 50.0 }
+      in
+          assertEqual
+          []
+          (errors [y1, y2, y3])
+
+    , test "Layers facing away from each other with > 100% should report error" <|
+      let
+          y1 = { prob = 0.51, limit = AtMost, value = 20.0 }
+          y2 = { prob = 0.60, limit = AtLeast, value = 50.0 }
+      in
+          assertEqual
+          [ MoreThan100Percent ]
+          (errors [y1, y2])
+
+    , test "Layers facing away from each other reversed with > 100% should report error" <|
+      let
+          y1 = { prob = 0.60, limit = AtLeast, value = 50.0 }
+          y2 = { prob = 0.51, limit = AtMost, value = 20.0 }
+      in
+          assertEqual
+          [ MoreThan100Percent ]
+          (errors [y1, y2])
+
+    , test "Layers facing away from each other with > 100% and hidden among others should report error" <|
+      let
+          y1 = { prob = 0.51, limit = AtMost, value = 20.0 }
+          y2 = { prob = 0.60, limit = AtLeast, value = 10.0 }
+          y3 = { prob = 0.55, limit = AtLeast, value = 50.0 }
+      in
+          assertEqual
+          [ MoreThan100Percent ]
+          (errors [y1, y2, y3])
+
     ]
 
