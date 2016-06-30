067f9867c8167090db671825adba771888395dba
Nik Silver <nik@niksilver.com>
2016-06-30 22:39:53 +0100

    Added H, h, V and v instructions for Path

--- src/Path.elm
+++ src/Path.elm
@@ -1,6 +1,6 @@
 module Path exposing
     ( Path (Path)
-    , Instruction (M, L, L')
+    , Instruction (M, M', L, L', H, H', V, V')
     , d
     )
 
@@ -9,9 +9,14 @@ module Path exposing
 
 
 type Instruction
-    = M Float Float
-    | L Float Float
+    = M  Float Float
+    | M' Float Float
+    | L  Float Float
     | L' Float Float
+    | H  Float
+    | H' Float
+    | V  Float
+    | V' Float
 
 type Path = Path (List Instruction)
 
@@ -27,14 +32,24 @@ d (Path instrs) =
 dInstr : Instruction -> String
 dInstr instr =
     case instr of
-        M x y ->
-            "M " ++ (toString x) ++ "," ++ (toString y)
-        L x y ->
-            "L " ++ (toString x) ++ "," ++ (toString y)
-        L' x y ->
-            "l " ++ (toString x) ++ "," ++ (toString y)
+        M  x y -> dTwoPart "M" x y
+        M' x y -> dTwoPart "m" x y
+        L  x y -> dTwoPart "L" x y
+        L' x y -> dTwoPart "l" x y
+        H  x -> dOnePart "H" x
+        H' x -> dOnePart "h" x
+        V  y -> dOnePart "V" y
+        V' y -> dOnePart "v" y
 
 maybeSpace : List Instruction -> String
 maybeSpace instrs =
     if (List.isEmpty instrs) then "" else " "
+
+dTwoPart : String -> Float -> Float -> String
+dTwoPart i x y =
+        i ++ " " ++ (toString x) ++ "," ++ (toString y)
+
+dOnePart : String -> Float -> String
+dOnePart i a =
+        i ++ " " ++ (toString a)
 
--- tests/PathTest.elm
+++ tests/PathTest.elm
@@ -39,10 +39,35 @@ dTest =
       "M 5,6 L 20,21"
       (Path [M 5 6, L 20 21] |> d)
 
+    , test "d for Path with lower case m should be right" <|
+      assertEqual
+      "m 21,22"
+      (Path [M' 21 22] |> d)
+
     , test "d for Path with lower case l should be right" <|
       assertEqual
       "l 20,21"
       (Path [L' 20 21] |> d)
+
+    , test "d for Path with H should be right" <|
+      assertEqual
+      "H 66"
+      (Path [H 66] |> d)
+
+    , test "d for Path with lower case h should be right" <|
+      assertEqual
+      "h 67"
+      (Path [H' 67] |> d)
+
+    , test "d for Path with V should be right" <|
+      assertEqual
+      "V 71"
+      (Path [V 71] |> d)
+
+    , test "d for Path with lower case v should be right" <|
+      assertEqual
+      "v 72"
+      (Path [V' 72] |> d)
 
     ]
 
