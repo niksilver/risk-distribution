c69fca3601af7a9b039ebdbc4a0b3026a7c08e27
Nik Silver <nik@niksilver.com>
2016-09-01 21:15:39 +0100

    Got initial version of getEntries working, for simple scenarios

--- src/ZoneDict.elm
+++ src/ZoneDict.elm
@@ -1,12 +1,28 @@
-module ZoneDict exposing (ZoneDict, Result)
+module ZoneDict exposing
+    ( Value (Exactly)
+    , getEntries
+    )
 
 import Zone exposing (Zone)
-import Error exposing (Error(Negative, Contradiction))
-
-import Dict exposing (Dict)
+import Constraint exposing (Constraint)
 
 
-type Result
-    = XXX
+-- Result of successive derivations, showing what we think the
+-- value is for a particular zone.
 
-type alias ZoneDict = Dict Zone Result
+type Value
+    = Exactly Int
+
+-- Get some entries for a ZoneDict.
+-- Given some zones and a constraint each entry shows the best result
+-- for each zone expressed in the constraint.
+
+getEntries : List Zone -> Constraint -> List (Zone, Value)
+getEntries zones cons =
+    let
+        includeZone zone coeff =
+            if (coeff == 0) then Nothing else Just zone
+    in
+        List.map2 includeZone zones cons.coeffs
+            |> List.filterMap identity
+            |> List.map (\z -> (z, Exactly cons.pc))
--- tests/ZoneDictTest.elm
+++ tests/ZoneDictTest.elm
@@ -2,10 +2,53 @@ module ZoneDictTest exposing (all)
 
 import ZoneDict exposing (..)
 
+import Zone exposing (Zone)
+import Constraint exposing (Constraint)
+
 import ElmTest exposing (..)
 
 all : Test
 all =
     suite "ZoneDictTest"
-    [
+    [ getEntriesTest
+    ]
+
+-- The tests...
+
+getEntriesTest : Test
+getEntriesTest =
+    suite "getEntriesTest"
+
+    [ test "Given no coeffs we should get no entries" <|
+      assertEqual
+      []
+      (getEntries
+        [Zone 0 1]
+        (Constraint [] 100)
+      )
+
+    , test "Given single coeff of 1 we should get one entry (1)" <|
+      assertEqual
+      [(Zone 5 7, Exactly 50)]
+      (getEntries
+        [Zone 5 7]
+        (Constraint [1] 50)
+      )
+
+    , test "Given single coeff of 1 we should get one entry (2)" <|
+      assertEqual
+      [(Zone 1 4, Exactly 66)]
+      (getEntries
+        [Zone 1 4]
+        (Constraint [1] 66)
+      )
+
+    , test "Given single coeff of 1 we should get no entries" <|
+      assertEqual
+      []
+      (getEntries
+        [Zone 5 7]
+        (Constraint [0] 50)
+      )
+
     ]
