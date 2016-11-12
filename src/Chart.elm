e05151ac1acaa400897fb15c6b104f1567a17587
Nik Silver <nik@niksilver.com>
2016-11-12 19:32:27 +0000

    Moved distribution curve code into its own module.

--- src/Chart.elm
+++ src/Chart.elm
@@ -1,13 +1,12 @@
 module Chart exposing (view)
 
+import Curve
+import Spline
 import FactList
 import Axis exposing (Scale)
 import Spec exposing (Spec, ViewDims, Transformer)
 import Block exposing (Rect)
 import Value exposing (Value (Exactly, Maximum, Contradiction))
-import Path exposing (Path (Path), Instruction (M, L))
-import Spline exposing (Pos)
-import Util
 
 import Html exposing (Html)
 import Svg exposing (Svg)
@@ -38,7 +37,7 @@ view spec =
     let
         -- Get the distribution curve and its min and max points
 
-        curve = distCurve spec
+        curve = Curve.distribution spec
         (curveMin, curveMax) =
             Spline.yMinMax curve
                 |> Maybe.withDefault (0, spec.maxY)
@@ -66,7 +65,7 @@ view spec =
         , SvgA.viewBox viewBoxDim
         ]
         [ viewBlocks transformer scaledSpec
-        --, viewCurve transformer curve
+        , Curve.view transformer curve
         , Axis.viewXAxis transformer scale
         ]
 
@@ -125,113 +124,3 @@ drawMaximum transformer rect pc =
         Svg.g
         []
         (List.map drawLine indices)
-
--- Render the distribution as a curve,
--- given functions to transform and the curve itself
-
-viewCurve : Transformer -> List Pos -> Svg x
-viewCurve transformer curve =
-    let
-        trans x y =
-            Pos (transformer.trX x) (transformer.trY y)
-        path =
-            curve
-                |> Path.fromPosList
-                |> Path.map trans
-    in
-        Svg.path
-        [ SvgA.d (Path.d path)
-        , SvgA.stroke "purple"
-        , SvgA.strokeWidth "2"
-        , SvgA.fill "rgba(124, 60, 155, 0.6)"
-        ]
-        []
-
--- Create a distribution curve
-
-distCurve : Spec -> List Pos
-distCurve spec =
-    let
-        rects = Spec.rects spec
-
-        -- To create the path for distribution curve we take
-        -- the initial set of rectangles in the chart spec and...
-        -- Put pretend rectangles at the start and end to
-        -- get a sense of where the curve should start and end from;
-        -- Take the rectangles in sliding groups of three and see
-        -- If the middle one is at a peak, in a dip, etc and add
-        -- curve points accordingly;
-        -- Join up the points with a spline.
-        -- Squash the curve up if it falls below the x-axis
-    in
-        rects
-            |> Spec.bracketRects
-            |> Util.sliding 3
-            |> List.map curvePoints
-            |> List.concat
-            |> addEndsOfSpline rects
-            |> Spline.splines 20
-            |> squash
-
--- Translate to Spec.curvePointsForRect, but taking a list
--- of three rectangles instead of three separate Rect arguments
-
-curvePoints : List Rect -> List Pos
-curvePoints rects =
-    case rects of
-        prev :: rect :: next :: [] ->
-            Spec.curvePointsForRect prev rect next
-        _ ->
-            []
-
--- Squash the curve up if it's below the x-axis
-
-squash : List Pos -> List Pos
-squash ps =
-    let
-        trans p =
-            Pos p.x (max p.y 0)
-    in
-        List.map trans ps
-
--- Add end points to the distribution curve
-
-addEndsOfSpline : List Rect -> List Pos -> List Pos
-addEndsOfSpline rects points =
-    points
-        |> addFrontOfSpline rects
-        |> addBackOfSpline rects
-
-addPosIfDifferent : Pos -> List Pos -> List Pos
-addPosIfDifferent p ps =
-    if (List.head ps == Just p) then
-        ps
-    else
-        p :: ps
-
-addFrontOfSpline : List Rect -> List Pos -> List Pos
-addFrontOfSpline rects points =
-    let
-        pos =
-            case List.head rects of
-                Nothing ->
-                    Pos 0 0
-                Just rect ->
-                    Pos rect.left 0
-    in
-        addPosIfDifferent pos points
-
-addBackOfSpline : List Rect -> List Pos -> List Pos
-addBackOfSpline rects points =
-    let
-        pos =
-            case (List.reverse rects |> List.head) of
-                Nothing ->
-                    Pos 0 0
-                Just rect ->
-                    Pos rect.right 0
-    in
-        points
-            |> List.reverse
-            |> addPosIfDifferent pos
-            |> List.reverse
